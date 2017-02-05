87101da1a1953bf79f98040bca4fb7e98dfe1a14
Tammy Xu <Tammy Xu>
2017-02-05 18:28:45 -0500

    Now I have mysql

--- SantasHelper/App/viewmodels/friends.js
+++ SantasHelper/App/viewmodels/friends.js
@@ -1,6 +1,28 @@
 ﻿define([], function () {
     var vm = {
-        
+        friends: [],
+        //so activate runs every time this page is selected? Isn't that inefficient if we do searchs on lots of users? 
+        //Better to save results in a data.js file?
+        activate: function () {
+            var self = this;
+            self.friends = [];
+            $.ajax({
+                type: "GET",
+                url: "/Durandal/GetAllUsers",
+                datatype: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    debugger;
+                    for (var i = 0; i < data.length; i++) {
+                        self.friends.push(data[i]);
+                    }
+                },
+                error: function () {
+                    debugger;
+                }
+
+            });
+        }
     };
     return vm;
 });
--- SantasHelper/Controllers/DurandalController.cs
+++ SantasHelper/Controllers/DurandalController.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace SantasHelper.Controllers {
@@ -9,7 +11,59 @@ namespace SantasHelper.Controllers {
         [HttpGet]
         public void CreateNewUser(string firstname, string lastname, string email, string password)
         {
+            string connectionString = "Server=localhost;Database=santashelper;Uid=root;Pwd=Ghmar01!;";
+            using (MySqlConnection mysql = new MySqlConnection(connectionString))
+            {
+                MySqlCommand createUser = mysql.CreateCommand();
+                createUser.CommandText = "INSERT INTO users (id, firstname, lastname, email, password) VALUES (NULL, ?f, ?l, ?e, ?p)";
+                createUser.Parameters.AddWithValue("?f", firstname);
+                createUser.Parameters.AddWithValue("?l", lastname);
+                createUser.Parameters.AddWithValue("?e", email);
+                createUser.Parameters.AddWithValue("?p", password);
 
+                mysql.Open();
+
+                try
+                {
+                    createUser.ExecuteNonQuery();
+                }
+                finally
+                {
+                    mysql.Close();
+                }
+
+            }
+        }
+
+        public JsonResult GetAllUsers()
+        {
+            List<string> userNames = new List<string>();
+
+            string connectionString = "Server=localhost;Database=santashelper;Uid=root;Pwd=Ghmar01!;";
+            using (MySqlConnection mysql = new MySqlConnection(connectionString))
+            {
+                MySqlCommand getUsersCmd = mysql.CreateCommand();
+                getUsersCmd.CommandText = "SELECT * FROM users";
+
+                mysql.Open();
+
+                MySqlDataReader reader = getUsersCmd.ExecuteReader();
+                try
+                {
+                    while(reader.Read())
+                    {
+                        userNames.Add(reader.GetString("firstname"));
+                    }
+                }
+                finally
+                {
+                    reader.Close();
+                    mysql.Close();
+                }
+
+            }
+
+            return Json(userNames, JsonRequestBehavior.AllowGet);
         }
     }
 }
